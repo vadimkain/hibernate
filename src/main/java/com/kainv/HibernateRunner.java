d8c89e88ba916c163d4bfe9954e3eb79a3f4e245
vadimkain <48841785+vadimkain@users.noreply.github.com>
2023-03-11 22:52:49 +0200

    005 Entity

--- src/main/java/com/kainv/HibernateRunner.java
+++ src/main/java/com/kainv/HibernateRunner.java
@@ -1,29 +1,42 @@
 package com.kainv;
 
+import com.kainv.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class HibernateRunner {
-    public static void main(String[] args) throws SQLException {
-//        BlockingQueue<Connection> pool = null;
-//        Connection connection = pool.take();
-//        SessionFactory
-
-//        Connection connection = DriverManager
-//                .getConnection("db.url", "db.username", "db.password");
-//        Session
-
+    public static void main(String[] args) {
         Configuration configuration = new Configuration();
+//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+//        configuration.addAnnotatedClass(User.class);
         configuration.configure();
+
+//        Failed to resolve org.junit.platform:junit-platform-launcher:1.7.2
+
+//        11:33
 
         try (
                 SessionFactory sessionFactory = configuration.buildSessionFactory();
                 Session session = sessionFactory.openSession()
         ) {
-            System.out.println("OK");
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("kainv@gmail.com")
+                    .firstname("Vadim")
+                    .lastname("Kain")
+                    .birthDate(LocalDate.of(2000, 1, 19))
+                    .age(20)
+                    .build();
+
+            session.save(user);
+
+            session.getTransaction().commit();
         }
     }
 }
